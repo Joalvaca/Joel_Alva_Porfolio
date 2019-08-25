2c7fcb14a30e9227e3251f840a3fbd1a9caed7b3
joel Alva <panthersjoe@hotmail.com>
2019-08-24 20:24:42 -0400

    addedjs

---
+++ index.js
@@ -0,0 +1,17 @@
+$('#Bio-link').click(function(){
+    $('html,body').animate({
+        scrollTop:$('#Bio-section').offset().top},
+        'slow');
+});
+
+$('#Projects-link').click(function(){
+    $('html,body').animate({
+        scrollTop:$('#Projects-section').offset().top},
+        'slow');
+});
+
+$('#Contact-link').click(function(){
+    $('html,body').animate({
+        scrollTop:$('#Contact-section').offset().top},
+        'slow');
+});
